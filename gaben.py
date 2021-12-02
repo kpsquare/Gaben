676ba9f6bb3d43174af34a8f07f10b8a5539e72b
kpsquare <krs.flp@gmail.com>
2021-12-02 18:55:18 +0000

    Update gaben.py

--- gaben.py
+++ gaben.py
@@ -1,3 +1,4 @@
+import aiohttp
 import discord
 import os
 from discord.ext import commands
@@ -26,10 +27,10 @@ async def gaben_listen(message):
                   '**MmmmMMm GABEEEEEN**',
                   '<:gaben:703198078836015154>',
                   '**GAAABEEN <:gaben:703198078836015154>**',
-                  '**GABEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEN**',
                   '**prysnat <:suspect:589057771933138945>**',
                   '||<:EDU:588721771373658123><:gaben:703198078836015154><:EDU:588721771373658123><:TOPKEK:588721771310743572>||',
-                  '||<:gaben:703198078836015154> FREE MMR <:gaben:703198078836015154>||'
+                  '||<:gaben:703198078836015154> FREE MMR <:gaben:703198078836015154>||',
+                  'gif'
                   ]
     streamables = re.compile('[g|G]+[a|A]+[b|B]+[e|E]+[n|N]+')
     
@@ -47,7 +48,14 @@ async def gaben_listen(message):
         return
     
     if match or similar > 60:
-        await message.reply(f'{"".join(random.choices(responses, weights = [10, 10, 10, 15, 15, 10, 10, 3, 1], k = 1))}', mention_author=False)
+        response = "".join(random.choices(responses, weights = [10, 10, 15, 15, 10, 10, 3, 2, 10], k = 1))
+        if response == 'gif':
+            ctx = await client.get_context(message)
+            embed=discord.Embed()
+            embed.set_image(url='https://media1.giphy.com/media/AcbborW0z5XW0/giphy.gif')
+            await ctx.send(embed=embed)
+        else:
+        await message.reply(f'{response}', mention_author=False)
 
 token = os.environ.get('api_key')        
 client.run(token)
